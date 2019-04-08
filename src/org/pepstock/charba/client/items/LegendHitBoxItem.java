e882db42608bdfe966052d8364bcebfcae2a58c8
stock <stocki.nail@gmail.com>
2019-04-08 17:22:50 +0200

    Remove this empty statement

--- src/org/pepstock/charba/client/items/LegendHitBoxItem.java
+++ src/org/pepstock/charba/client/items/LegendHitBoxItem.java
@@ -67,7 +67,6 @@ public final class LegendHitBoxItem extends SizeItem {
 			boolean isX = event.getLayerX() >= getLeft() && event.getLayerX() <= (getLeft() + getWidth());
 			// checks Y
 			boolean isY = event.getLayerY() >= getTop() && event.getLayerY() <= (getTop() + getHeight());
-			;
 			return isX && isY;
 		}
 		// if here, no properties, then false
