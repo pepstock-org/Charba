3874db9af270ea69fceb66f26348a2d5120f4029
stock <stocki.nail@gmail.com>
2020-10-27 12:13:01 +0100

    adds missing fields to get from CHART.JS object

--- src/org/pepstock/charba/client/items/DatasetMetaItem.java
+++ src/org/pepstock/charba/client/items/DatasetMetaItem.java
@@ -27,7 +27,9 @@ import org.pepstock.charba.client.commons.IsEnvelop;
 import org.pepstock.charba.client.commons.Key;
 import org.pepstock.charba.client.commons.NativeObject;
 import org.pepstock.charba.client.commons.NativeObjectContainer;
+import org.pepstock.charba.client.commons.ObjectType;
 import org.pepstock.charba.client.enums.DefaultScaleId;
+import org.pepstock.charba.client.enums.IndexAxis;
 import org.pepstock.charba.client.options.IsScaleId;
 
 /**
@@ -48,12 +50,22 @@ public final class DatasetMetaItem extends NativeObjectContainer {
 		DATA("data"),
 		HIDDEN("hidden"),
 		INDEX("index"),
+		INDEX_AXIS("indexAxis"),
 		LABEL("label"),
 		ORDER("order"),
 		TYPE("type"),
 		VISIBLE("visible"),
 		Y_AXIS_ID("yAxisID"),
 		X_AXIS_ID("xAxisID"),
+		R_AXIS_ID("rAxisID"),
+		V_AXIS_ID("vAxisID"),
+		I_AXIS_ID("iAxisID"),
+		// FIXME
+		Y_SCALE("yScale"),
+		X_SCALE("xScale"),
+		R_SCALE("rScale"),
+		V_SCALE("vScale"),
+		I_SCALE("iScale"),
 		// doughnut, pie
 		TOTAL("total");
 
@@ -164,6 +176,15 @@ public final class DatasetMetaItem extends NativeObjectContainer {
 	public int getIndex() {
 		return getValue(Property.INDEX, UndefinedValues.INTEGER);
 	}
+	
+	/**
+	 * Returns the dataset index axis.
+	 * 
+	 * @return the dataset index axis or <code>null</code> if not found.
+	 */
+	public IndexAxis getIndexAxis() {
+		return getValue(Property.INDEX_AXIS, IndexAxis.values(), null);
+	}
 
 	/**
 	 * Returns the dataset order.
@@ -191,6 +212,15 @@ public final class DatasetMetaItem extends NativeObjectContainer {
 	public IsScaleId getYAxisID() {
 		return getValue(Property.Y_AXIS_ID, DefaultScaleId.Y);
 	}
+	
+	/**
+	 * Returns the Y scale item or <code>null</code> if not exists.
+	 * 
+	 * @return the Y scale item or <code>null</code> if not exists
+	 */
+	public ScaleItem getYScale() {
+		return retrieveScale(Property.Y_SCALE);
+	}
 
 	/**
 	 * Returns the X axis ID.
@@ -199,6 +229,69 @@ public final class DatasetMetaItem extends NativeObjectContainer {
 	 */
 	public IsScaleId getXAxisID() {
 		return getValue(Property.X_AXIS_ID, DefaultScaleId.X);
+	}
+
+	/**
+	 * Returns the X scale item or <code>null</code> if not exists.
+	 * 
+	 * @return the X scale item or <code>null</code> if not exists
+	 */
+	public ScaleItem getXScale() {
+		return retrieveScale(Property.X_SCALE);
+	}
+
+	/**
+	 * Returns the R axis ID.
+	 * 
+	 * @return the R axis ID. Default is {@link DefaultScaleId#R}.
+	 */
+	public IsScaleId getRAxisID() {
+		return getValue(Property.R_AXIS_ID, DefaultScaleId.R);
+	}
+	
+	/**
+	 * Returns the R scale item or <code>null</code> if not exists.
+	 * 
+	 * @return the R scale item or <code>null</code> if not exists
+	 */
+	public ScaleItem getRScale() {
+		return retrieveScale(Property.R_SCALE);
+	}
+	
+	/**
+	 * Returns the V axis ID.
+	 * 
+	 * @return the V axis ID. Default is {@link DefaultScaleId#Y}.
+	 */
+	public IsScaleId getVAxisID() {
+		return getValue(Property.V_AXIS_ID, DefaultScaleId.Y);
+	}
+	
+	/**
+	 * Returns the V scale item or <code>null</code> if not exists.
+	 * 
+	 * @return the V scale item or <code>null</code> if not exists
+	 */
+	public ScaleItem getVScale() {
+		return retrieveScale(Property.V_SCALE);
+	}
+	
+	/**
+	 * Returns the I axis ID.
+	 * 
+	 * @return the I axis ID. Default is {@link DefaultScaleId#X}.
+	 */
+	public IsScaleId getIAxisID() {
+		return getValue(Property.I_AXIS_ID, DefaultScaleId.X);
+	}
+	
+	/**
+	 * Returns the I scale item or <code>null</code> if not exists.
+	 * 
+	 * @return the I scale item or <code>null</code> if not exists
+	 */
+	public ScaleItem getIScale() {
+		return retrieveScale(Property.I_SCALE);
 	}
 
 	/**
@@ -218,6 +311,22 @@ public final class DatasetMetaItem extends NativeObjectContainer {
 	public List<DatasetItem> getDatasets() {
 		ArrayObject array = getArrayValue(Property.DATA);
 		return ArrayListHelper.unmodifiableList(array, DatasetItem.FACTORY);
+	}
+	
+	/**
+	 * Returns the scale item from a specific property.
+	 * 
+	 * @param key object property to use
+	 * @return a scael item or <code>null</code> if not found
+	 */
+	private ScaleItem retrieveScale(Property key) {
+		// checks if the property is consistent
+		if (Key.isValid(key) && isType(key, ObjectType.OBJECT)) {
+			// creates the scale item
+			return new ScaleItem(getValue(key));
+		}
+		// if here, the key or teh value is not consistent
+		return null;
 	}
 
 }
