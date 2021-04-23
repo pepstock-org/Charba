6d3a2f1f706c9bfae7793f7d785d97c1026ce65a
stock <stocki.nail@gmail.com>
2021-04-23 22:26:15 +0200

    re-implements padding extending the abstract one in the options

--- src/org/pepstock/charba/client/datalabels/Padding.java
+++ src/org/pepstock/charba/client/datalabels/Padding.java
@@ -15,30 +15,22 @@
 */
 package org.pepstock.charba.client.datalabels;
 
-import org.pepstock.charba.client.callbacks.PaddingCallback;
 import org.pepstock.charba.client.commons.NativeObject;
 import org.pepstock.charba.client.defaults.IsDefaultPadding;
-import org.pepstock.charba.client.enums.Position;
-import org.pepstock.charba.client.options.IsPadding;
+import org.pepstock.charba.client.options.AbstractPadding;
 
 /**
- * Base object to map font options for {@link DataLabelsPlugin#ID} plugin configuration.<br>
- * It is applied to all sides of the chart (left, top, right, bottom).<br>
- * It can be used also in the callback for padding generation at runtime.
+ * Base object to map padding options for {@link DataLabelsPlugin#ID} plugin configuration.<br>
+ * It is applied to all sides of the label (left, top, right, bottom).
  * 
  * @author Andrea "Stock" Stocchero
- * @see PaddingCallback
  */
-public final class Padding extends AbstractElement implements IsPadding {
+public final class Padding extends AbstractPadding {
 
 	/**
-	 * FIXME
 	 * Default padding, <b>{@value DEFAULT_PADDING}</b>.
 	 */
 	public static final int DEFAULT_PADDING = 4;
-
-	// defaults global options instance
-	private IsDefaultPadding defaultOptions;
 
 	/**
 	 * Creates new padding element, using stored native object instance and the default values options.
@@ -47,89 +39,7 @@ public final class Padding extends AbstractElement implements IsPadding {
 	 * @param defaultOptions default PADDING options to returns the default when required.
 	 */
 	Padding(IsDefaultPadding defaultOptions, NativeObject nativeObject) {
-		super(nativeObject);
-		// checks if default value is consistent
-		// stores default
-		this.defaultOptions = checkDefaultValuesArgument(defaultOptions);
+		super(defaultOptions, nativeObject);
 	}
 
-	/**
-	 * Sets the padding left in pixel.
-	 * 
-	 * @param padding the padding left in pixel.
-	 */
-	@Override
-	public void setLeft(int padding) {
-		setValue(Position.LEFT, padding);
-	}
-
-	/**
-	 * Returns the padding left in pixel.
-	 * 
-	 * @return the padding left in pixel.
-	 */
-	@Override
-	public int getLeft() {
-		return getValue(Position.LEFT, defaultOptions.getLeft());
-	}
-
-	/**
-	 * Sets the padding right in pixel.
-	 * 
-	 * @param padding the padding right in pixel.
-	 */
-	@Override
-	public void setRight(int padding) {
-		setValue(Position.RIGHT, padding);
-	}
-
-	/**
-	 * Returns the padding right in pixel.
-	 * 
-	 * @return the padding right in pixel.
-	 */
-	@Override
-	public int getRight() {
-		return getValue(Position.RIGHT, defaultOptions.getRight());
-	}
-
-	/**
-	 * Sets the padding top in pixel.
-	 * 
-	 * @param padding the padding top in pixel.
-	 */
-	@Override
-	public void setTop(int padding) {
-		setValue(Position.TOP, padding);
-	}
-
-	/**
-	 * Returns the padding top in pixel.
-	 * 
-	 * @return the padding top in pixel.
-	 */
-	@Override
-	public int getTop() {
-		return getValue(Position.TOP, defaultOptions.getTop());
-	}
-
-	/**
-	 * Sets the padding bottom in pixel.
-	 * 
-	 * @param padding the padding bottom in pixel.
-	 */
-	@Override
-	public void setBottom(int padding) {
-		setValue(Position.BOTTOM, padding);
-	}
-
-	/**
-	 * Returns the padding bottom in pixel.
-	 * 
-	 * @return the padding bottom in pixel.
-	 */
-	@Override
-	public int getBottom() {
-		return getValue(Position.BOTTOM, defaultOptions.getBottom());
-	}
 }
