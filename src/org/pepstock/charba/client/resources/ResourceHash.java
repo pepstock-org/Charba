bab18faa584beafc7f812f64eb275a3edc3de032
stockiNail <stocki.nail@gmail.com>
2021-11-17 11:37:29 +0100

    imports Annotation plugin version 1.1.0

--- src/org/pepstock/charba/client/resources/ResourceHash.java
+++ src/org/pepstock/charba/client/resources/ResourceHash.java
@@ -29,7 +29,7 @@ enum ResourceHash {
 	/**
 	 * Hash item for 'AnnotationPluginResource' class.
 	 */
-	ANNOTATION_PLUGIN_RESOURCE(ResourceName.ANNOTATION_PLUGIN, 1243091),
+	ANNOTATION_PLUGIN_RESOURCE(ResourceName.ANNOTATION_PLUGIN, 1300903),
 	/**
 	 * Hash item for 'ChartJsResource' class.
 	 */
