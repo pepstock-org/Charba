5dee487c95db12cb18c355586ca468280062b527
stockiNail <stocki.nail@gmail.com>
2022-07-26 19:47:53 +0200

    import CHART.JS GEO controller version v3.8.1

--- src/org/pepstock/charba/client/resources/ResourceHash.java
+++ src/org/pepstock/charba/client/resources/ResourceHash.java
@@ -41,7 +41,7 @@ enum ResourceHash {
 	/**
 	 * Hash item for 'GeoControllerResource' class.
 	 */
-	GEO_CONTROLLER_RESOURCE(ResourceName.GEO_CONTROLLER, 9022098),
+	GEO_CONTROLLER_RESOURCE(ResourceName.GEO_CONTROLLER, 9077257),
 	/**
 	 * Hash item for 'GradientPluginResource' class.
 	 */
