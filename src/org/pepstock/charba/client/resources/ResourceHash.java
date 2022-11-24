8f22b1dba71493e342d5922ab3dca0196b72ed23
stockiNail <stocki.nail@gmail.com>
2022-11-24 11:37:38 +0100

    import CHART.JS ANNOTATION plugin version v2.1.0

--- src/org/pepstock/charba/client/resources/ResourceHash.java
+++ src/org/pepstock/charba/client/resources/ResourceHash.java
@@ -29,7 +29,7 @@ enum ResourceHash {
 	/**
 	 * Hash item for 'AnnotationPluginResource' class.
 	 */
-	ANNOTATION_PLUGIN_RESOURCE(ResourceName.ANNOTATION_PLUGIN, 2593416),
+	ANNOTATION_PLUGIN_RESOURCE(ResourceName.ANNOTATION_PLUGIN, 2610800),
 	/**
 	 * Hash item for 'ChartJsResource' class.
 	 */
