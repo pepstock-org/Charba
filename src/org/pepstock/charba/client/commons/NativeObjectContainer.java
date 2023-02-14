fe30521e92ca9a015337deb071a039a70301345d
stockiNail <stocki.nail@gmail.com>
2023-02-14 17:52:19 +0100

    Fix #87

--- src/org/pepstock/charba/client/commons/NativeObjectContainer.java
+++ src/org/pepstock/charba/client/commons/NativeObjectContainer.java
@@ -565,7 +565,7 @@ public abstract class NativeObjectContainer {
 			// gets descriptor
 			double value = NativeObjectUtil.getDoubleProperty(nativeObject, key.value(), Undefined.DOUBLE);
 			// checks if value is consistent
-			if (Checker.isGreaterThan(value, 1D)) {
+			if (Checker.isGreaterThan(value, 0D)) {
 				// creates and returns a date
 				return new ImmutableDate((long) value);
 			}
